f4637531146995e3d6dc43fc7fc903bea775cdbe
vizapatel <155963255+vizapatel@users.noreply.github.com>
2024-04-04 10:59:19 +0530

    Add files via upload

---
+++ register.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<Head>
+    <title>Registration Page</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row col-md-6 col-md-offset-3">
+            <div class="panel panel-primary">
+                <div class="panel-heading">
+                    This is heading
+                </div>  
+                <div class="panel-body">
+                    This is body
+                </div>
+                <div class="panel-footer">
+                    This is footer                        
+                </div>
+            </div>  
+        </div>
+    </div>
+</body>
+</html>                 
